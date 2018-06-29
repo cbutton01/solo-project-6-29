dc378910887632ebefaf80ca71582e25395f213c
cb <csbutton01@hotmail.com>
2018-06-29 14:48:58 -0700

    add styling and links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
-  const navbar = {
+  const NAVBAR = {
+    backgroundColor: '#DC191B',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
     fontFamily: 'sans-serif',
     padding: '0 25px',
     minWidth: '625px',
-    color: '#333',
-    borderBottom: '2px solid #ccc'
+    color: 'white'
   };
-  const navBtnGroup = {
+  const HEADER = {
+    color: 'white',
+    backgroundColor: 'black',
+    padding: '2%',
     display: 'flex',
-    listStyle: 'none',
-    padding: '0'
+    justifyContent: 'space-between'
   };
-  const navBtn = {
-    border: '1px solid gray',
-    padding: '10px 20px',
-    marginLeft: '-1px'
-  };
-  const navItem = {
-    border: '2px solid #2795dc',
-    borderRadius: '15px',
-    padding: '10px 20px',
-    marginLeft: '20px',
-    fontSize: '14px'
-  };
+  const TITLE = {
+    display: 'flex'
+  }
+  const RED = {
+    backgroundColor: '#DC191B'
+  }
 
   return (
-    <div style={navbar}>
-      <ul style={navBtnGroup}>
-        <li style={navBtn}>Home</li>
-        <li style={navBtn}>Notifications</li>
-        <li style={navBtn}>Messages</li>
-      </ul>
-      <div>
-        <input style={navItem} placeholder="Search"></input>
-        <button style={navItem}>Tweet</button>
+    <div>
+      <div style={HEADER}>
+        <div style={TITLE}>
+          <h1>
+            <span style={RED}>PC</span>GAMER
+            </h1>
+            <h5>THE GLOBAL AUTHORITY ON PC GAMES</h5>
+        </div>
+        <p>SOCIAL MEDIA ICONS</p>
+      </div>
+      <div style={NAVBAR}>
+        <Link to='/nowhere'>
+          <h6>News</h6>
+        </Link>
+        <Link to='/nowhere'>
+          <h6>Hardware</h6>
+        </Link>
+        <Link to='/nowhere'>
+          <h6>Indie</h6>
+        </Link>
+        <Link to='/nowhere'>
+          <h6>Pro</h6>
+        </Link>
+        <Link to='/nowhere'>
+          <h6>Best Of</h6>
+        </Link>
+        <Link to='/nowhere'>
+          <h6>Magazine</h6>
+        </Link>
+        <Link to='/nowhere'>
+          <h6>Board Games</h6>
+        </Link>
+        <h6><label>More<select>
+          <option value=""></option>
+          <option value="">Podcasts</option>
+          <option value="">Meet the team</option>
+          <option value="">Newsletter Signup</option>
+          <option value="">Community Guidelines</option>
+          <option value="">Affiliate Links</option>
+          <option value="">About PC Gamer</option>
+        </select></label></h6>
       </div>
     </div>
   );
